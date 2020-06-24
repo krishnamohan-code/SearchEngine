ea8d10309de98a13dc47019bf945a1680cdb0fdf
krishnamohan <krishnamohan.py@gmail.com>
2020-06-24 19:55:49 +0530

    Task completed

---
+++ index.js
@@ -0,0 +1,13 @@
+function myfunction()
+{
+searchvalue=document.getElementById("search").value;
+// console.log(searchvalue);
+if(searchvalue==="birds")
+{
+      window.location="images.html"
+}
+else if(searchvalue==="animals")
+{
+    window.location="animals.html"
+}
+}
