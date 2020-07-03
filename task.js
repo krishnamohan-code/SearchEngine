4e65a91716da8da4ee9e8955b2157b698f8337b1
krishnamohan <krishnamohan.py@gmail.com>
2020-07-03 20:07:52 +0530

    SearchEngine

--- task.js
+++ task.js
@@ -1,5 +1,5 @@
 $("#search").click(function(){
-    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+   $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
     {
       tags: $("#searchterm").val(),
       tagmode: "any",
@@ -8,7 +8,27 @@ $("#search").click(function(){
     function(data) {
       $.each(data.items, function(i,item){
         $("<img/>").attr("src", item.media.m).prependTo("#results");
-        if ( i == 100 ) return false;
+       
+        if ( i == 100) return false;
       });
     });
   });
+  // document.getElementById("search").addEventListener(onclick,function()
+  // {
+  //   document.getJSON(
+  //     "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+  //     {
+  //       tags:getElementById("searchterm").val(),
+  //       tagmode:"any",
+  //       format:"json"
+  //     },
+  //     function(data)
+  //     {
+  //       document.each(data.items,function(i,item)
+  //       {
+  //         document.createElement('img').setAttribute("src",item.media.m);
+  //         if ( i == 100) return false;
+  //       });
+  //     }
+  //   )
+  // });
