f0bbd27e6832533a21764c72b2592287962cb5db
krishnamohan <krishnamohan.py@gmail.com>
2020-07-03 21:37:40 +0530

    completed

--- task.js
+++ task.js
@@ -1,36 +1,19 @@
-/* J-Query*/
+var name;
 $("#search").click(function(){
+  name = $("#searchterm").val();
+  window.location.href = "result.html";
+});
+$(document).ready(function(){
    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
     {
-      tags: $("#searchterm").val(),
+      tags: name,
       tagmode: "any",
       format: "json"
     },
     function(data) {
       $.each(data.items, function(i,item){
         $("<img/>").attr("src", item.media.m).prependTo("#results");
-       
         if ( i == 100) return false;
       });
     });
   });
-  /* Java Script */
-  // document.getElementById("search").addEventListener(onclick,function()
-  // {
-  //   document.getJSON(
-  //     "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-  //     {
-  //       tags:getElementById("searchterm").val(),
-  //       tagmode:"any",
-  //       format:"json"
-  //     },
-  //     function(data)
-  //     {
-  //       document.each(data.items,function(i,item)
-  //       {
-  //         document.createElement('img').setAttribute("src",item.media.m);
-  //         if ( i == 100) return false;
-  //       });
-  //     }
-  //   )
-  // });
